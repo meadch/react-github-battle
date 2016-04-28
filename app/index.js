fdb7644a06b169c48602888e4e945755300e9041
meadch <meadch@gmail.com>
2016-04-28 14:29:51 -0700

    initial commit w/ dependencies

---
+++ app/index.js
@@ -0,0 +1,14 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+var HelloWorld = React.createClass({
+  render: function(){
+    return (
+      <div>
+        Hello World!
+      </div>
+    )
+  }
+});
+
+//we’re using ReactDOM.render and not React.render. This was a change made in React .14 to make React more modular. 
+ReactDOM.render(<HelloWorld />, document.getElementById('app'));
