45500dcdf5442fe45d4cbcf2bd65d974fd19e048
meadch <meadch@gmail.com>
2016-04-28 20:47:46 -0700

    pure function notes

--- app/index.js
+++ app/index.js
@@ -1,14 +1,31 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 var HelloWorld = React.createClass({
+  //React's render method needs to be a pure function
   render: function(){
+    var friends = ["Janel", "Dan", "Hank"]
     return (
       <div>
-        Hello World!
+        Hello {this.props.name}!
+        <ShowList names={friends} />
       </div>
     )
   }
 });
 
-//we’re using ReactDOM.render and not React.render. This was a change made in React .14 to make React more modular. 
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+//The whole concept of a pure function is consistency and predictability
+/*
+- Pure functions always return the same result given the same arguments.
+- Pure function's execution doesn't depend on the state of the application.
+- Pure functions don't modify the variables outside of their scope.
+*/
+var ShowList = function(props){
+  var friends = props.names.map( (friend, idx) => ( <li key={idx}>{friend}</li> ) )
+  return (
+    <ul>
+      {friends}
+    </ul>
+  )
+}
+
+ReactDOM.render(<HelloWorld name="Charlie" />, document.getElementById('app'));
