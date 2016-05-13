36ef989bba4433e787916238f6c1530d7f34d608
meadch <meadch@gmail.com>
2016-05-13 13:05:19 -0700

    adds routing animations

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,10 +1,20 @@
-const React = require('react')
+const React = require('react');
+// We want styling in this file to be applied to the component in this file
+// Need to configure Webpack for this and bring in transition group
+const ReactCSSTransitionGroup = require('react-addons-css-transition-group')
+require('../main.css');
 
 const Main = React.createClass({
   render () {
     return (
       <div className='main-container'>
-        {this.props.children}
+        <ReactCSSTransitionGroup
+          transitionName="appear"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}>
+          {/*this.props.children needs a key to fit w/ transition, so we have to use cloneElement to generate a component that we provide a key to*/}
+          {React.cloneElement(this.props.children, {key: this.props.location.pathname})}
+        </ReactCSSTransitionGroup>
       </div>
     )
   }
