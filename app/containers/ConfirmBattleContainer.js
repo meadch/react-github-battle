42654a55fc4a1ad58bed799c542d79d566a2d178
meadch <meadch@gmail.com>
2016-05-17 11:49:08 -0700

    adds class syntax

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,18 +1,17 @@
-import React, {PropTypes} from 'react'
+import React, {PropTypes, Component} from 'react'
 import ConfirmBattle from '../components/ConfirmBattle'
 import {getPlayersInfo} from '../utils/githubHelpers'
 
-const ConfirmBattleComponent = React.createClass({
-  contextTypes: {
-    router: PropTypes.object.isRequired
-  },
+class ConfirmBattleComponent extends Component {
+
   // Components needs to know whether loading or not
-  getInitialState () {
-    return {
+  constructor () {
+    super()
+    this.state = {
       isLoading: true,
       playersInfo: []
     }
-  },
+  }
   async componentDidMount () {
     // Grab usernames and fetch infor from GitHub API
     const { query } = this.props.location
@@ -33,7 +32,7 @@ const ConfirmBattleComponent = React.createClass({
         playersInfo: []
       })
     }
-  },
+  }
   handleStartBattle () {
     this.context.router.push({
       pathname: '/results',
@@ -41,15 +40,20 @@ const ConfirmBattleComponent = React.createClass({
         playersInfo: this.state.playersInfo
       }
     })
-  },
+  }
   render () {
+    // Because handleStartBattle will be called in the context of the ConfirmBattle component, we need to wrap in an arrow function to protect this.
     return (
       <ConfirmBattle
         isLoading={this.state.isLoading}
         playersInfo={this.state.playersInfo}
-        onStartBattle={this.handleStartBattle} />
+        onStartBattle={() => this.handleStartBattle() } />
     );
   }
-});
+}
+
+ConfirmBattleComponent.contextTypes = {
+  router: PropTypes.object.isRequired
+}
 
 export default ConfirmBattleComponent
